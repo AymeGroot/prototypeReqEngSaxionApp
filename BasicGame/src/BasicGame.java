1136735b6f84dd4fcc94942898875edce20b9ceb
aymegroot <563995@student.saxion.nl>
2025-03-18 12:29:20 +0100

    mainmenu

--- BasicGame/src/BasicGame.java
+++ BasicGame/src/BasicGame.java
@@ -1,3 +1,4 @@
+import MainMenu.MainMenu;
 import nl.saxion.app.SaxionApp;
 
 import nl.saxion.app.interaction.GameLoop;
@@ -9,9 +10,11 @@ public class BasicGame implements GameLoop {
     public static void main(String[] args) {
         SaxionApp.startGameLoop(new BasicGame(), 1000, 1000, 40);
     }
+    MainMenu mainMenu = new MainMenu();
 
     @Override
     public void init() {
+
 
     }
 
